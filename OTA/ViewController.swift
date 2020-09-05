a2dd0167084cac6b6c615b3169e23c1e84e29201
Joey Zhang <joey.zhang@tgitech.net>
2020-09-05 22:53:34 +0800

    feat: init project

---
+++ OTA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OTA
+//
+//  Created by Yi Zhang on 2020/9/5.
+//  Copyright © 2020 Yi Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
